e8606f270998b953e8b5c1f06181e7ffcaa56b0a
Administrator <admin@example.com>
2016-07-06 17:03:59 +0530

    basic assertion test written

--- src/test.js
+++ src/test.js
@@ -1,4 +1,15 @@
 (function(){
 	"use strict";
 	
+	assertEquals(8,add(4,3));
+	
+	function add(a,b){
+		return a+b;
+	}
+	
+	function assertEquals(actual,expected)
+	{
+		if(actual!==expected) throw new Error("Expected "+expected+" but got "+actual);
+	}
+	
 }());
