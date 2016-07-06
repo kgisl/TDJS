e5d75add1b276cfef6b3b402566c26dfc29a983f
Administrator <admin@example.com>
2016-07-06 18:25:40 +0530

    mocha test written

--- src/test.js
+++ src/test.js
@@ -3,9 +3,16 @@
 	
 	var assert=require("chai").assert;
 	
-	assert.equal(add(4,3),7);
-	
-	assert.equal(add(0.1,0.2),0.30000000000000004);
+	describe("Addition",function(){
+		
+		it("adds positive Numbers",function(){
+			assert.equal(add(4,3),7);
+		});
+		
+		it("uses IEE 754 floating point",function(){
+			assert.equal(add(0.1,0.2),0.30000000000000004);
+		});
+	});
 	
 	function add(a,b){
 		return a+b;
